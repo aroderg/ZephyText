5216007b27e408dde501325ed923b7f8e4fb903f
aroderg <anothersoul1staken@gmail.com>
2025-12-19 01:25:12 +0200

    changed string.split function to return weights

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-local teststng = io.open("string.txt"):read("*all")
-local weights = {}
+local teststng = io.open("string.txt"):read("*all"):lower():gsub('[%p%c]', '')
 function string.split(input, separator)
     separator = separator or "%s"
     local matched = string.gmatch(input, '([^'..separator..']+)')
     local numerated = {}
     local numeratedBigrams = {}
+    local weights = {}
     for str in matched do
         table.insert(numerated, str)
     end
@@ -13,11 +13,18 @@ function string.split(input, separator)
             table.insert(numeratedBigrams, numerated[i-1] .. " " .. (v or ""))
         end
     end
-    return numeratedBigrams
+    for i,v in ipairs(numeratedBigrams) do
+        if not weights[v] then
+            weights[v] = 0
+        else
+            weights[v] = weights[v] + 1
+        end
+    end
+    return weights
 end
 
-local numeratedBigrams = string.split(teststng)
+local weights = string.split(teststng)
 
-for i,v in pairs(numeratedBigrams) do
-    print(i .. ". " .. v)
+for i,v in pairs(weights) do
+    print(i .. ": " .. v)
 end
